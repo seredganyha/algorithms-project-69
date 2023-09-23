b5997565bfdc439fcd0264e4a050889651d04644
seredganyha <karix05@yandex.ru>
2023-09-24 01:01:16 +0300

    drop debug code

--- src/search.js
+++ src/search.js
@@ -3,7 +3,6 @@ export default function search(docs, string) {
 
   return docs
     .map((doc) => {
-      console.log(doc.text.split(' ').map((el) => el.replace(/[^a-zа-яё]/ig, '')));
       const textSet = new Set(doc.text.split(' ').map((el) => el.replace(/[^a-zа-яё]/ig, '')));
       if (textSet.has(string.replace(/[^a-zа-яё]/ig, ''))) return doc.id;
       return null;
